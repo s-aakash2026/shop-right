247abf0bd3309c546443802a2fd9d0f30070a272
s-aakash2026 <saakash2026@gmail.com>
2025-12-26 19:13:28 +0530

    Replace Locomotive Scroll with GSAP ScrollTrigger
    Removed Locomotive Scroll integration and updated to use standard scrolling with GSAP ScrollTrigger.

--- src/hooks/useLocoScroll.js
+++ src/hooks/useLocoScroll.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import LocomotiveScroll from "locomotive-scroll";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
@@ -9,40 +8,8 @@ export default function useLocoScroll(start) {
   useEffect(() => {
     if (!start) return;
 
-    const scrollEl = document.querySelector("#main-container");
-    
-    const locoScroll = new LocomotiveScroll({
-      el: scrollEl,
-      smooth: true,
-      multiplier: 1,
-      class: "is-reveal",
-    });
-
-    locoScroll.on("scroll", ScrollTrigger.update);
-
-    ScrollTrigger.scrollerProxy(scrollEl, {
-      scrollTop(value) {
-        return arguments.length
-          ? locoScroll.scrollTo(value, 0, 0)
-          : locoScroll.scroll.instance.scroll.y;
-      },
-      getBoundingClientRect() {
-        return {
-          top: 0,
-          left: 0,
-          width: window.innerWidth,
-          height: window.innerHeight,
-        };
-      },
-      pinType: scrollEl.style.transform ? "transform" : "fixed",
-    });
-
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    // Locomotive Scroll has been removed
+    // Standard scroll with GSAP ScrollTrigger is used instead
     ScrollTrigger.refresh();
-
-    return () => {
-      if (locoScroll) locoScroll.destroy();
-      ScrollTrigger.getAll().forEach((t) => t.kill());
-    };
   }, [start]);
 }
